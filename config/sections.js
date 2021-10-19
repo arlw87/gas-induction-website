48e504139720b32115722e32c13dd1a41f42ede5
arlw87 <arlw@googlemail.com>
2021-10-19 15:11:47 +0100

    Update section config file to include section tag
    Refactor Non-Clincal page to extract title and tag from sections
    config file and pass to the SectionHeader component as props.

--- config/sections.js
+++ config/sections.js
@@ -1,26 +1,28 @@
 const sections = [
   {
+    id: 1,
     sectionName: "Clinical Induction",
     link: "clinical-induction",
+    tag: "Resources to aid first few weeks in anaesthesia",
   },
   {
+    id: 2,
     sectionName: "Anaesthetics Recipes",
     link: "anaesthetic-recipes",
+    tag: "Reference guides to providing anaesthesia for common operations",
   },
   {
+    id: 3,
     sectionName: "Technical Tips",
     link: "technical-tips",
+    tag: "Basic advice on machine checks, airway equipment, opening glass vials, making antibiotics etc.",
   },
   {
+    id: 4,
     sectionName: "Non Clinical Work",
     link: "non-clincal",
+    tag: "Advice on the Lifelong Learning platform and exams",
   },
 ];
 
 export default sections;
-
-// "Anaesthetics",
-// "Technical-Tips",
-// "Non-clinical-Work",
-// "About-Us",
-// ];
--- pages/non-clincal/index.js
+++ pages/non-clincal/index.js
@@ -1,12 +1,19 @@
 import SectionHeader from "../../components/UI/SectionHeader";
+import sections from "../../config/sections";
+
+//Do not change this
+const sectionID = 4;
 
 const NonClinicalWorkSection = () => {
-  return (
-    <SectionHeader
-      title="Non Clinical"
-      tag="Advice on the Lifelong Learning platform and exams"
-    ></SectionHeader>
+  //Get Section Title and Tagline
+  //As this is for a static generated website no need to useEffect
+  //to run on mount
+
+  const { sectionName, tag } = sections.find(
+    (section) => section.id === sectionID
   );
+
+  return <SectionHeader title={sectionName} tag={tag}></SectionHeader>;
 };
 
 export default NonClinicalWorkSection;
