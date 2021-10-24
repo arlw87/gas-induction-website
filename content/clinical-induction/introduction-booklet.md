187509a617adf832ea546a9d75face8398e115f2
arlw87 <arlw87@googlemail.com>
2021-10-24 01:05:32 +0100

    Set up clinical induction section
    Add Section page, with content cards and the dynamic pages that
    include the content

--- content/clinical-induction/introduction-booklet.md
+++ content/clinical-induction/introduction-booklet.md
@@ -4,7 +4,7 @@ type: pdf
 date: 24/10/2021
 summary: A quick reference guide to beginning anaesthetics. Includes summary tables of common drugs, basics of the machine and circle system in addition to suggested reading topics.
 description: A quick reference guide to beginning anaesthetics. Includes summary tables of common drugs, basics of the machine and circle system in addition to suggested reading topics.
-media:
+media: "/media/clinical-induction/PDF/Anaesthetic-Introduction-Booklet.pdf"
 ---
 
 # Acknowledgements
--- pages/clinical-induction/index.js
+++ pages/clinical-induction/index.js
@@ -1,26 +1,41 @@
 import SectionHeader from "../../components/UI/SectionHeader";
 import sections from "../../config/sections";
 import { useRouter } from "next/router";
-//fs wont work in the client side, but getStaticProps runs on the server side, so if it is
-//used in the getStaticProps function there wont be an error
-//import fs from "fs";
-//import path from "path";
+import ContentCard from "../../components/UI/ContentCard";
+import SubjectSection from "../../components/Layout/SubjectSection";
+import {
+  getSectionDetails,
+  returnGetStaticProps,
+} from "../../lib/helperFunctions";
+import path from "path";
+import matter from "gray-matter";
+import fs from "fs";
 
-//Do not change this
-const sectionID = 1;
-
-const NonClinicalWorkSection = (props) => {
+const ClinicalInductionPage = (props) => {
   //Get Section Title and Tagline
   //As this is for a static generated website no need to useEffect
   //to run on mount
 
   //get the currentRoute
   const router = useRouter();
+
+  //console.log(router);
+
   const url = router.pathname.slice(1);
 
-  const { sectionName, tag, color } = sections.find(
-    (section) => section.link === url
-  );
+  const { sectionName, tag, color } = getSectionDetails(url);
+
+  const cards = props.pages.map((page, index) => {
+    return (
+      <ContentCard
+        link={`/${url}/${page.slug}`}
+        title={page.frontMatter.title}
+        summary={page.frontMatter.summary}
+        type={page.frontMatter.type}
+        key={index}
+      ></ContentCard>
+    );
+  });
 
   return (
     <>
@@ -29,20 +44,42 @@ const NonClinicalWorkSection = (props) => {
         tag={tag}
         backgroundColor={color}
       ></SectionHeader>
-      <section>{props.title}</section>
+      <SubjectSection>{cards}</SubjectSection>
     </>
   );
 };
 
-export default NonClinicalWorkSection;
+export default ClinicalInductionPage;
 
-export const getStaticProps = async () => {
-  //fetch markup
-  //const files = fs.readdirSync(path.join("content"));
+export async function getStaticProps() {
+  const sectionName = "clinical-induction";
+
+  //get files from the content/non-clinical folder
+  const files = fs.readdirSync(path.join("content", sectionName));
+
+  //get pages and front matter
+  const pages = files.map((filename) => {
+    const slug = filename.replace(".md", "");
+
+    //get front matter
+    const markdownWithMatter = fs.readFileSync(
+      path.join("content", sectionName, filename),
+      "utf-8"
+    );
+
+    const { data: frontMatter } = matter(markdownWithMatter);
+
+    return {
+      slug,
+      frontMatter,
+    };
+  });
+
+  //console.log("pages", pages);
 
   return {
     props: {
-      title: "Hello World",
+      pages,
     },
   };
-};
+}
