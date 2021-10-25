b05d2d30075b465274e5ba551019765e24f43860
arlw87 <arlw87@googlemail.com>
2021-10-25 19:37:02 +0100

    Style: NavBar for Desktop Styling
    Add viewport variables in viewport.css, to make breakpoints
    consistent

--- components/Layout/NavBar/NavBar.js
+++ components/Layout/NavBar/NavBar.js
@@ -4,25 +4,30 @@ import NavLink from "../../UI/NavLink";
 
 const NavBar = (props) => {
   return (
-    <nav className={classes.nav}>
-      <div className={classes.title}>
-        <div className={classes.iconContainer}>
-          <img className={classes.icon} src={props.icon} alt="logo" />
+    <div className={classes.largeScreenWrapper}>
+      <nav className={classes.nav}>
+        <div className={classes.title}>
+          <div className={classes.iconContainer}>
+            <img className={classes.icon} src={props.icon} alt="logo" />
+          </div>
+          <Link href="/">Gas Induction</Link>
         </div>
-        <Link href="/">Gas Induction</Link>
-      </div>
-      <ul className={classes.navLinksSection}>
-        {props.sections.map((section, index) => {
-          return (
-            <li key={index} className={classes.navLinks}>
-              <NavLink href={`/${section.link}`} hightlighColor={section.color}>
-                {section.sectionName}
-              </NavLink>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
+        <ul className={classes.navLinksSection}>
+          {props.sections.map((section, index) => {
+            return (
+              <li key={index} className={classes.navLinks}>
+                <NavLink
+                  href={`/${section.link}`}
+                  hightlighColor={section.color}
+                >
+                  {section.sectionName}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </div>
   );
 };
 
