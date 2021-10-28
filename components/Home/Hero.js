283e985f57dbbcc826776fef14dc7b3cd3a48f66
arlw87 <arlw87@googlemail.com>
2021-10-28 23:16:32 +0100

    Style: Responsive homepage hero

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -6,19 +6,21 @@ const Hero = (props) => {
   };
 
   return (
-    <div className={classes.heroContainer}>
-      <div className={classes.textContainer}>
-        <div className={classes.textWrapper}>
-          <h1>Gas Induction</h1>
-          <p>
-            Video lectures, written resources and useful links for anaesthetic
-            novices
-          </p>
-          <button onClick={btnClickHandler}>Start Learning</button>
+    <div className={classes.heroWrapper}>
+      <div className={classes.heroContainer}>
+        <div className={classes.textContainer}>
+          <div className={classes.textWrapper}>
+            <h1>Gas Induction</h1>
+            <p>
+              Video lectures, written resources and useful links for anaesthetic
+              novices
+            </p>
+            <button onClick={btnClickHandler}>Start Learning</button>
+          </div>
         </div>
-      </div>
-      <div className={classes.imageContainer}>
-        <div className={classes.heroImage}></div>
+        <div className={classes.imageContainer}>
+          <div className={classes.heroImage}></div>
+        </div>
       </div>
     </div>
   );
